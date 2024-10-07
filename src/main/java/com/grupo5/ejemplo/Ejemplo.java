d5683e8081a0af9e7921994a071b1136faaf006a
Charlie <charlie.cordova34@gmial.com>
2024-10-07 18:48:18 -0500

    Cambio de Charlie

--- src/main/java/com/grupo5/ejemplo/Ejemplo.java
+++ src/main/java/com/grupo5/ejemplo/Ejemplo.java
@@ -13,9 +13,14 @@ public class Ejemplo {
     public static void main(String[] args) {
         System.out.println("Curso git");
         printJosue();
+        printCharlie();
     }
     
     public static void printJosue(){
         System.out.println("Curso de git, att Josue");
+    }
+    
+    public static void printCharlie(){
+        System.out.println("Curso de git, att Charlie");
     }
 }
