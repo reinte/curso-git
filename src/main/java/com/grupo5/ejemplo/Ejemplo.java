b09688ff74e332f82f8ddfd74bba324047d198fc
jossuema <mmalla1@outlook.com>
2024-10-07 11:53:09 -0500

    Creando proyecto por Josue

---
+++ src/main/java/com/grupo5/ejemplo/Ejemplo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.grupo5.ejemplo;
+
+/**
+ *
+ * @author mmall
+ */
+public class Ejemplo {
+
+    public static void main(String[] args) {
+        System.out.println("Curso git");
+        printJosue();
+    }
+    
+    public static void printJosue(){
+        System.out.println("Curso de git, att Josue");
+    }
+}
